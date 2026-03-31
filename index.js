4fc95b8eaa4142fbe42aa9e4646fb514099fb42f
Pranab kundu <kundupranab17@gmail.com>
2026-03-31 11:36:03 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const response = await fetch("https://api-inference.huggingface.co/models/google/flan-t5-small", {
-  method: "POST",
-  headers: {"Content-Type": "application/json"},
-  body: JSON.stringify({inputs: q})
-});
